8db297745d301e464b480c4ee35cca316a0a2c99
coollx <coollx@me.com>
2022-09-30 03:36:45 -0400

    updated stuff

--- utils.py
+++ utils.py
@@ -1,3 +1,9 @@
+import string
+from spellchecker import SpellChecker
+
+#cache the spell checker
+spell = SpellChecker()
+
 def elem2sent(article, break_sentence = True):
     '''
     Break article into sentences. Break sentences with "."
@@ -10,5 +16,26 @@ def elem2sent(article, break_sentence = True):
     if break_sentence:
         sentences = [sent for sub in map(lambda x: x.split('.'), sentences) for sent in sub if len(sent.split()) > 5]
     
+    sentences = [sent.strip().lower().translate(str.maketrans('', '', string.punctuation)) for sent in sentences]
+
+    #remove the indices of sentences
+    sentences = [sent.split(' ', 1)[1] if len(sent[0]) <= 3 else sent for sent in sentences]
+
+    #correct spellings
+    #sentences = [correct_spellings(sent) for sent in sentences]
+
     #remove empty sentences
     return list(filter(None, sentences))
+
+
+
+def correct_spellings(text):
+    corrected_text = []
+    misspelled_words = spell.unknown(text.split())
+    for word in text.split():
+        if word in misspelled_words:
+            corrected_text.append(spell.correction(word))
+        else:
+            corrected_text.append(word)
+    return " ".join(list(filter(None, corrected_text)))
+        
